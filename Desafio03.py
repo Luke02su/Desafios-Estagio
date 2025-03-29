a453cf0bf49dde37397aba7723719206d71e5cb7
Lucas <68867449+Luke02su@users.noreply.github.com>
2025-03-29 10:59:48 -0300

    Desafio03.py
    3) Faça um algoritmo para ler 50 números e armazenar em uma lista, verificar e escrever se existem números repetidos na lista e em que posições se encontram.

--- Desafio03.py
+++ Desafio03.py
@@ -1,17 +1,30 @@
-def calcular_notas(lista_notas):
-    notas_finais = []
-    for nota in lista_notas:
-        if nota < 38:
-            notas_finais.append(nota)
-        else:
-            proximo_multiplo = nota + (5 - nota % 5)
-            if proximo_multiplo - nota < 3:
-                notas_finais.append(proximo_multiplo)
-            else:
-                notas_finais.append(nota)
-    return notas_finais
+import random
+import array
 
-notas = [73, 67, 38, 33]
+array = array.array('i')
 
-notas_finalizadas = calcular_notas(notas)
-print("Notas finais:", notas_finalizadas)
+for i in range(50):
+    numerosAleatorios = random.randint(0, 100)
+    array.append(numerosAleatorios)
+
+lista = list(array)
+
+posicoes = {}
+
+for idx, num in enumerate(lista):
+    if num in posicoes:
+        posicoes[num].append(idx)
+    else:
+        posicoes[num] = [idx]
+
+repetidos = {num: pos for num, pos in posicoes.items() if len(pos) > 1}
+
+if repetidos:
+    print('Números repetidos e suas posições:')
+    for num, pos in repetidos.items():
+        print('Número', num, 'aparece nas posições', pos)
+else:
+    print('Não há números repetidos.')
+
+print('\nLista de números gerados:')
+print(lista)
